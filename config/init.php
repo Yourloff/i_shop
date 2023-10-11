6ad82495f33cfa48d8590172b29f7134e38231bc
Yourloff <alex.yourloff@gmail.com>
2023-10-12 00:17:41 +0700

    a registry has been created

--- config/init.php
+++ config/init.php
@@ -5,7 +5,7 @@ define("ROOT", dirname(__DIR__));
 define("WWW", ROOT . '/public');
 define("APP", ROOT . '/app');
 define("CORE", ROOT . '/vendor/ishop/core');
-define("LIBS", ROOT . '/vendor/ishop/libs');
+define("LIBS", ROOT . '/vendor/ishop/core/libs');
 define("CACHE", ROOT . '/tmp/cache');
 define("CONF", ROOT . '/config');
 define("LAYOUT", 'default');
